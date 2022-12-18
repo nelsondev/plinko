eedee284d414aab35b6b5714fff9548563cf2c52
Ben Talbot <btalbot00@gmail.com>
2022-12-18 16:42:44 -0700

    More power bar shit

--- scenes/End.gd
+++ scenes/End.gd
@@ -11,9 +11,18 @@ func _process(delta):
 		var parent = get_node("../../")
 		var camera = get_node("../../Camera2D")
 		var ships = get_node("../../../Ships")
+		var power_bar = get_node("../../Control/PowerBar")
+		var power_side_bar = get_node("../../Control/SideBar")
 		
 		parent.modulate.a = lerp(parent.modulate.a, 0.3, 0.05)
 		ships.modulate.a = lerp(ships.modulate.a, 1.0, 0.05)
+		power_bar.visible = false
+		for n in range(0, 3):
+			power_side_bar.visible = true
+			yield(get_tree().create_timer(1), "timeout")
+			power_side_bar.visible = false
+			print(n)
+			
 		camera.zoom = camera.zoom.linear_interpolate(Vector2(1.2, 1.2), 0.1)
 
 func _on_End_body_entered(body):
--- scenes/PowerBar.gd
+++ scenes/PowerBar.gd
@@ -2,6 +2,8 @@ extends Control
 
 onready var TweenNode = get_node("PowerBar/Tween")
 var current_charge = 0
+var charge_level = 0
+
 
 func _ready():
 	Game.connect("charge_added", self, "_animate_power_bar")
@@ -13,5 +15,11 @@ func _animate_power_bar():
 	TweenNode.interpolate_property($PowerBar, "value", current_charge, charge_factor, 0.5, Tween.TRANS_SINE, Tween.EASE_OUT)
 	TweenNode.start()
 	
+func _on_Tween_tween_all_completed():
+	var charge_factor = Game.charge*5
 	current_charge = charge_factor
-
+	print(current_charge)
+	if (current_charge % 20) == 0:
+		charge_level += 1
+		print(charge_level)
+		$SideBar.value = charge_level
